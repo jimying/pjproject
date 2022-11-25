aa5d330742318adb523d58fd37719e7dfe4b891b
jimying <yingqw.js@gmail.com>
2022-11-25 13:52:06 +0800

    Add build ,debug script

---
+++ s.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+BUILD_OUT=$PWD/../build
+JOBS=$(nproc)
+OS=$(uname -s |  tr '[:upper:]' '[:lower:]')
+
+case $OS in
+    *bsd*)
+        MAKE=gmake
+        ;;
+    *)
+        MAKE=make
+        ;;
+esac
+
+#export CFLAGS="-g"
+export CFLAGS="-DNDEBUG -fPIC -Wno-unused-label -Wextra -Wno-missing-field-initializers"
+$MAKE distclean
+./configure --prefix=$BUILD_OUT/pjsip \
+    --enable-epoll \
+    --disable-libuuid \
+    --disable-gsm-codec \
+    --disable-speex-codec --disable-speex-aec \
+    --disable-l16-codec \
+    --disable-g722-codec \
+    --disable-g7221-codec \
+    --disable-ilbc-codec \
+    --disable-bcg729 \
+    --disable-silk \
+    --disable-video \
+    --disable-libwebrtc \
+    --disable-sound \
+    --disable-opus \
+    --disable-ssl \
+    --disable-upnp \
+    --disable-libsrtp
+    #--with-opus=$BUILD_OUT/opus
+
+rm -f pjlib/include/pj/config_site.h
+cp -f config_site.h pjlib/include/pj
+$MAKE dep
+#$MAKE -C pjlib/build -j  $JOBS
+#$MAKE -C pjlib-util/build -j  $JOBS
+#$MAKE -C pjnath/build -j  $JOBS
+$MAKE -j $JOBS
+#$MAKE install
