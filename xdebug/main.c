edecbd735bc8a7b5e74c3362c6c291d821528624
jimying <yingqw.js@gmail.com>
2022-12-07 21:12:42 +0800

    Fix crash

--- xdebug/main.c
+++ xdebug/main.c
@@ -5,6 +5,8 @@
 #define DELAY_MS_Q 10
 #define DELAY_MS 20
 #define DEFAULT_THREAD_CNT 1
+#define TIMER_ID_RUN 1
+#define TIMER_ID_STOPED -1
 
 struct global_info
 {
@@ -43,6 +45,21 @@ static void timer_cb(pj_timer_heap_t *timer_heap, struct pj_timer_entry *entry)
     pj_timer_heap_schedule(timer_heap, entry, &delay);
 }
 
+static int timer_cb2(pj_timer_heap_t *timer_heap, struct pj_timer_entry *entry)
+{
+    static pj_uint64_t cnt = 0;
+    cnt++;
+    if (cnt % 50 == 0)
+        PJ_LOG(4, (THIS_FILE, "%s() cnt:%d", __FUNCTION__, cnt));
+    if (cnt == 1000)
+    {
+        /* stop */
+        entry->id = TIMER_ID_STOPED;
+        return 0;
+    }
+    return DELAY_MS;
+}
+
 int main(int argc, char **argv)
 {
     pj_status_t status;
@@ -51,6 +68,7 @@ int main(int argc, char **argv)
     pj_pool_t *pool;
     pj_ioqueue_t *ioq;
     pj_timer_heap_t *ht;
+    pj_lock_t *lock;
     pj_timer_entry timer;
     pj_time_val delay = {0, DELAY_MS};
     int thread_cnt = DEFAULT_THREAD_CNT;
@@ -75,6 +93,9 @@ int main(int argc, char **argv)
     status = pj_timer_heap_create(pool, 64, &ht);
     PJ_ASSERT_RETURN(status == PJ_SUCCESS, status);
 
+    pj_lock_create_recursive_mutex(pool, NULL, &lock);
+    pj_timer_heap_set_lock(ht, lock, PJ_TRUE);
+
     pj_bzero(&global, sizeof(global));
     global.ioq = ioq;
     global.ht = ht;
@@ -87,7 +108,8 @@ int main(int argc, char **argv)
         pj_thread_create(pool, "work%p", work_proc, &global, 0, 0, &global.threads[i]);
     }
 
-    pj_timer_entry_init(&timer, 0, &global, timer_cb);
+    pj_timer_entry_init(&timer, TIMER_ID_RUN, &global, timer_cb);
+    pj_timer_entry_init2(&timer, TIMER_ID_RUN, &global, timer_cb2);
     pj_timer_heap_schedule(ht, &timer, &delay);
 
     PJ_LOG(4, (THIS_FILE, "input 'q' to quit"));
