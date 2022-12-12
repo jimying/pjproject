77c6ac263586df3d2b8b29164127a26dc94bc1cf
jimying <yingqw.js@gmail.com>
2022-12-12 09:29:04 +0800

    Add build script and config

---
+++ config_site.h
@@ -0,0 +1,8 @@
+#   define PJSIP_MAX_TSX_COUNT		((32*1024)-1)
+#   define PJSIP_MAX_DIALOG_COUNT	((32*1024)-1)
+#define PJ_LOG_MAX_LEVEL 5
+
+// debug
+#define PJ_DEBUG 1
+#define PJ_TIMER_DEBUG 1
+
